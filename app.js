fedadee415aa5ee00a983745f1c9c77b198f2433
kou <kou.pryvate9511ok@gmail.com>
2025-07-06 00:20:28 +0900

    完了タスクのグレーアウト表示を追加

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ $(addTodo).click(function() {
 $(document).on("change", "input[type=checkbox]", function() {
     if ($(this).is(":checked")){
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "gray");
     }else{
         $(this).parent().css("text-decoration", "none");
+        $(this).parent().css("color", "black");
     }
 });
